5a94c72ade33f7d2b95c954f0c35a4f110653119
Georgefdz <georgfdz@gmail.com>
2024-06-23 14:30:03 -0600

    Fix: img not displaying properly

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -15,11 +15,11 @@ function RecentProjects() {
             {projects.map(({ id, title, des, img, iconLists, link }) => (
                 <div key={id} className=' sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
                     <PinContainer title={link} href={link}>
-                        <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] lg:h-[30vh] mb-10'>
+                        <div className='relative sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] lg:h-[30vh] mb-10'>
                             <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                                 <img src="/bg.png" alt="bg-img" />
                             </div>
-                            <div>
+                            <div className='flex justify-center'>
                                 <img src={img} alt={title} className='z-10 absolute bottom-0'/>
                             </div>
                         </div>
